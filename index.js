520a82f75717f39955678745f5b062a9ac45c728
Kai Shovlin <kai.k.b.shovlin@gmail.com>
2019-01-15 13:12:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var price = Math.floor(100*(Math.random()));
+ var price = Math.floor(Math.random()) * 100;
  cart.push({[item] : price})
 }
 
